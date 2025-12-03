9fe34e8c58750e60c3e3944fc9e42186971ae2e1
Mariana <mariana.caceres.job@gmail.com>
2025-12-03 13:09:10 -0300

    Repositorio limpio

--- src/entidades/Estado.java
+++
@@ -1,7 +0,0 @@
-package entidades;
-
-public class Estado {
-    public static final String activo="ACTIVO";
-    public static final String pendiente="PENDIENTE";
-    public static final String finalizado="FINALIZADO";
-}
